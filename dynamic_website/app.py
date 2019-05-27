8ad9d6d81880f3b31d774a35aa6aee588d91230a
anjing <anjingcuc@gmail.com>
2019-05-27 23:20:09 +0800

    实现滚动加载版本。

--- dynamic_website/app.py
+++ dynamic_website/app.py
@@ -1,7 +1,7 @@
 # -*- coding=utf8 -*-
 
 # 从 flask 中导入一个可以用于实例化 WSGI app的类
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os
 
 # 实例化这个 WSGI 类
@@ -9,8 +9,8 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/data')
+def data():
   page = request.args.get('page', default=1)
 
   current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,15 +19,21 @@ def index():
   students = []
   with open(filename, encoding='utf8') as f:
     for line in f:
-      students.append(line)
+      students.append(line.strip())
 
-  start = (page - 1) * 20
-  end = len(students) if page * 20 > len(students) else page * 20
+  start = (int(page) - 1) * 20
+  end = len(students) if int(page) * 20 > len(students) else int(page) * 20
 
-  return render_template('index.html', students=students[start:end])
+  result = {'data': students[start:end]}
+  return jsonify(result)
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
 
 
 # __name__ 取值为 __main__ 说明当前py是程序的主入口
 # 否则，说明当前py是以导入包的方式运行的
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
--- dynamic_website/static/scripts.js
+++ dynamic_website/static/scripts.js
@@ -1,1 +1,43 @@
 // TODO some function
+let page = 1
+
+
+function getData(data) {
+  return data.data.map(function (item) {
+      return '\n<li>\n' + item +'</li>\n';
+  }).join('');
+}
+
+
+new Scrollload({
+  // container 和 content 两个配置的默认取的scrollload-container和scrollload-content类的dom。只要你按照以上的dom结构写，这两个配置是可以省略的
+  container: document.querySelector('.scrollload-container'),
+  content: document.querySelector('.scrollload-content'),
+  loadMore: function (sl) {
+
+    if (page === 5) {
+      // 没有数据的时候需要调用noMoreData
+      sl.noMoreData()
+      return
+    }
+
+    // 我这里用jquery的不是因为需要jquery，只是jquery的语法看起来比较简单。大家都认识。
+    // 如果你不是用jquery，可以看看原生的insertAdjacentHTML方法来替代append
+    $.ajax({
+      type: 'GET',
+      url: `/data?page=${page++}`,
+      dataType: 'json',
+      success: function (data) {
+        // contentDom其实就是你的scrollload-content类的dom
+        $(sl.contentDom).append(getData(data))
+
+        // 处理完业务逻辑后必须要调用unlock
+        sl.unLock()
+      },
+      error: function (xhr, type) {
+        // 加载出错，需要执行该方法。这样底部DOM会出现出现异常的样式。
+        sl.throwException()
+      }
+    })
+  }
+})
